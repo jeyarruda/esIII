b4d86ba23d6d020139f4a98af591159e1ead43ce
Eduardo Nunes <edugenix@gmail.com>
2019-09-29 13:53:43 -0300

    adicionado validador de cpf

--- ArquivoBancario/src/br/com/pageseguro/Run.java
+++ ArquivoBancario/src/br/com/pageseguro/Run.java
@@ -22,7 +22,7 @@ public class Run {
 		
 		int op;
 		do {
-			System.out.print("==============\n"
+			System.out.print("=================\n"
 				+ "1 - Remessas em JSON\n"
 				+ "2 - Remessas em XML\n"
 				+ "=================\n>");
@@ -42,12 +42,14 @@ public class Run {
 					break;
 			}
 		} while(op != 1 && op != 2);
+		sc.close();
 		listarRemessas(remessas);
 	}
 	
 	public static void listarRemessas(List<Remessa> remessas) {
-		System.out.println("============== Pagamentos XML =============");
+		System.out.println("================= Remessas ================");
 		for(Remessa r : remessas) {
+			
 			if(r instanceof RemessaBoleto) {
 				System.out.println("---| Pagamento via boleto |---");
 				RemessaBoleto rb = (RemessaBoleto) r;
@@ -69,7 +71,7 @@ public class Run {
 				System.out.println("[Número Cartão] = " + rcd.getNumeroCartao());
 			}
 			
-			System.out.println("[CPF] = " + r.getCPF());
+			System.out.printf("[CPF] = %s %s\n", r.getCPF(), validarCPF(r.getCPF())? "(Válido)":"(Inválido)");
 			System.out.println("[Nome] = " + r.getNome());
 			System.out.println("[Banco Pagamento] = " + r.getBancoPagamento());
 			System.out.println("[Banco Recebimento] = " + r.getBancoPagamento());
@@ -78,5 +80,28 @@ public class Run {
 			System.out.println("--- -------------------------------- ---");
 		}
 		System.out.println("=========================================");
+	}
+
+	public static boolean validarCPF(String cpf) {
+		int[] pesos = {10,9,8,7,6,5,4,3,2};
+		int[] npesos = {11,10,9,8,7,6,5,4,3,2};
+		char[] cpfArray = cpf.toCharArray();
+		int soma = 0, primeiroDigito, segundoDigito;
+		
+		for(int i = 0; i < 9; i++) {
+			soma += Character.getNumericValue(cpfArray[i]) * pesos[i];
+		}
+		
+		primeiroDigito = soma % 11 < 2 ? 0 : 11 - soma % 11;
+		
+		soma = 0;
+		for(int i = 0; i < 10; i++) {
+			soma += Character.getNumericValue(cpfArray[i]) * npesos[i];
+		}
+		
+		segundoDigito = soma % 11 < 2 ? 0 : 11 - soma % 11;
+		
+		return (Character.getNumericValue(cpfArray[9]) == primeiroDigito && 
+				Character.getNumericValue(cpfArray[10]) == segundoDigito);
 	}
 }
